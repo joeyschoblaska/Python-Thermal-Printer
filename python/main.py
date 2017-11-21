e531ed2656ddbfb161f55e6b581f0012f80d15a6
Joey Schoblaska <joey.schoblaska@gmail.com>
2017-11-21 13:55:49 -0600

    remove cruft from main.py

--- python/main.py
+++ python/main.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python
 
-# Main script for Adafruit Internet of Things Printer 2.  Monitors button
-# for taps and holds, performs periodic actions (Twitter polling by default)
-# and daily actions (Sudoku and weather by default).
-# Written by Adafruit Industries.  MIT license.
-#
 # MUST BE RUN AS ROOT (due to GPIO access)
 #
 # Required software includes Adafruit_Thermal, Python Imaging and PySerial
@@ -24,11 +19,7 @@ ledPin       = 18
 buttonPin    = 23
 holdTime     = 2     # Duration for button hold (shutdown)
 tapTime      = 0.01  # Debounce time for button taps
-nextInterval = 0.0   # Time of next recurring operation
-dailyFlag    = False # Set after daily trigger occurs
-lastId       = '1'   # State information passed to/from interval script
 printer      = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
-
 
 # Called when button is briefly tapped.  Invokes time/temperature script.
 def tap():
@@ -36,35 +27,14 @@ def tap():
   subprocess.call(["python", "timetemp.py"])
   GPIO.output(ledPin, GPIO.LOW)
 
-
 # Called when button is held down.  Prints image, invokes shutdown process.
 def hold():
   GPIO.output(ledPin, GPIO.HIGH)
   printer.printImage(Image.open('gfx/goodbye.png'), True)
-  printer.feed(3)
+  printer.feed(4)
   subprocess.call("sync")
   subprocess.call(["shutdown", "-h", "now"])
   GPIO.output(ledPin, GPIO.LOW)
-
-
-# Called at periodic intervals (30 seconds by default).
-# Invokes twitter script.
-def interval():
-  GPIO.output(ledPin, GPIO.HIGH)
-  p = subprocess.Popen(["python", "twitter.py", str(lastId)],
-    stdout=subprocess.PIPE)
-  GPIO.output(ledPin, GPIO.LOW)
-  return p.communicate()[0] # Script pipes back lastId, returned to main
-
-
-# Called once per day (6:30am by default).
-# Invokes weather forecast and sudoku-gfx scripts.
-def daily():
-  GPIO.output(ledPin, GPIO.HIGH)
-  subprocess.call(["python", "forecast.py"])
-  subprocess.call(["python", "sudoku-gfx.py"])
-  GPIO.output(ledPin, GPIO.LOW)
-
 
 # Initialization
 
@@ -82,25 +52,16 @@ GPIO.output(ledPin, GPIO.HIGH)
 # stalling during greeting.
 time.sleep(30)
 
-# Show IP address (if network is available)
+# make sure network is reachable
 try:
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	s.connect(('8.8.8.8', 0))
-	printer.print('My IP address is ' + s.getsockname()[0])
-	printer.feed(3)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 0))
 except:
-	printer.boldOn()
-	printer.println('Network is unreachable.')
-	printer.boldOff()
-	printer.print('Connect display and keyboard\n'
-	  'for network troubleshooting.')
-	printer.feed(3)
-	exit(0)
-
-# Print greeting image
-printer.printImage(Image.open('gfx/hello.png'), True)
-printer.feed(3)
-GPIO.output(ledPin, GPIO.LOW)
+    printer.boldOn()
+    printer.println('Network is unreachable.')
+    printer.boldOff()
+    printer.feed(4)
+    exit(0)
 
 # Poll initial button state and time
 prevButtonState = GPIO.input(buttonPin)
@@ -145,23 +106,3 @@ while(True):
     GPIO.output(ledPin, GPIO.HIGH)
   else:
     GPIO.output(ledPin, GPIO.LOW)
-
-  # Once per day (currently set for 6:30am local time, or when script
-  # is first run, if after 6:30am), run forecast and sudoku scripts.
-  l = time.localtime()
-  if (60 * l.tm_hour + l.tm_min) > (60 * 6 + 30):
-    if dailyFlag == False:
-      daily()
-      dailyFlag = True
-  else:
-    dailyFlag = False  # Reset daily trigger
-
-  # Every 30 seconds, run Twitter scripts.  'lastId' is passed around
-  # to preserve state between invocations.  Probably simpler to do an
-  # import thing.
-  if t > nextInterval:
-    nextInterval = t + 30.0
-    result = interval()
-    if result is not None:
-      lastId = result.rstrip('\r\n')
-
