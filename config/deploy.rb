1b1f51de973bba83636f204fc25f6a2784156f79
Joey Schoblaska <joey.schoblaska@gmail.com>
2017-11-21 22:31:56 -0600

    fix and rename println cap task

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,8 +6,10 @@ set :deploy_to, "/home/pi/pi-printer"
 set :public_children, []
 set :use_sudo, false
 
-task :invoke do
+task :println do
+  line = ARGV.drop(2).join(" ")
+
   on roles(:app) do
-    execute "cd #{current_path} && python python/println.py #{ARGV.join(" ")}"
+    execute "cd #{current_path} && python python/println.py #{line}"
   end
 end
