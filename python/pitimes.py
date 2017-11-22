aa38a2c76d0c377fc96425d85cf3e21c91065830
Joey Schoblaska <joey.schoblaska@gmail.com>
2017-11-21 22:44:02 -0600

    feed paper after printing pitimes

--- python/pitimes.py
+++ python/pitimes.py
@@ -9,3 +9,5 @@ printer.println("Cook County commissioners approve 2018 budget with over 300 lay
 
 printer.setSize("S")
 printer.println("CHICAGO - The Cook County commissioners unanimously passed the 2018 budget Tuesday, cutting $200 million and over 300 jobs in the process.")
+
+printer.feed(4)
