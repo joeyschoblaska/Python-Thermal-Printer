444b558b722af4941b16bb089c0fcd0eeef0507c
Joey Schoblaska <joey.schoblaska@gmail.com>
2017-11-21 22:53:55 -0600

    more hardcoded news

--- python/pitimes.py
+++ python/pitimes.py
@@ -4,12 +4,22 @@ from Adafruit_Thermal import *
 
 printer = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
 
-printer.boldOn()
-printer.underlineOn()
-printer.println("Cook County commissioners approve 2018 budget with over 300 layoffs")
-printer.boldOff()
-printer.underlineOff()
+def printHeadline(headline):
+    printer.boldOn()
+    printer.underlineOn()
+    printer.println(headline)
+    printer.boldOff()
+    printer.underlineOff()
 
+printHeadline("Cook County commissioners approve 2018 budget with over 300 layoffs")
 printer.println("CHICAGO - The Cook County commissioners unanimously passed the 2018 budget Tuesday, cutting $200 million and over 300 jobs in the process.")
+
+printer.feed(2)
+
+printer.println("The F.C.C. unveiled a plan to repeal net neutrality rules, which require broadband providers to give consumers equal access to all content on the web.")
+
+printer.feed(2)
+
+printer.println("Nancy Pelosi, the House Democratic leader, has called on the Ethics Committee to investigate sexual harassment charges against John Conyers Jr., the House’s longest-serving member.")
 
 printer.feed(4)
