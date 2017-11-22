9b2d5859c9041db8e7312854ee068eb95d0717e2
Joey Schoblaska <joey.schoblaska@gmail.com>
2017-11-21 22:46:10 -0600

    try bold instead of changing size for headlines

--- python/pitimes.py
+++ python/pitimes.py
@@ -4,10 +4,10 @@ from Adafruit_Thermal import *
 
 printer = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
 
-printer.setSize("M")
+printer.boldOn()
 printer.println("Cook County commissioners approve 2018 budget with over 300 layoffs")
+printer.boldOff()
 
-printer.setSize("S")
 printer.println("CHICAGO - The Cook County commissioners unanimously passed the 2018 budget Tuesday, cutting $200 million and over 300 jobs in the process.")
 
 printer.feed(4)
