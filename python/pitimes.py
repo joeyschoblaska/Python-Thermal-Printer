aa2c64f1d353f8d99f2c12bab2127694ac6157c1
Joey Schoblaska <joey.schoblaska@gmail.com>
2017-11-21 22:49:18 -0600

    underline headers too?

--- python/pitimes.py
+++ python/pitimes.py
@@ -5,8 +5,10 @@ from Adafruit_Thermal import *
 printer = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
 
 printer.boldOn()
+printer.underlineOn()
 printer.println("Cook County commissioners approve 2018 budget with over 300 layoffs")
 printer.boldOff()
+printer.underlineOff()
 
 printer.println("CHICAGO - The Cook County commissioners unanimously passed the 2018 budget Tuesday, cutting $200 million and over 300 jobs in the process.")
 
